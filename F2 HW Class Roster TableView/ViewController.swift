fb70344587a60f0d5a6c8c2d2a8cb59d9b88db44
Matthew Loh <mattloh111@gmail.com>
2015-06-09 19:53:37 -0700

    Initial Commit

---
+++ F2 HW Class Roster TableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  F2 HW Class Roster TableView
+//
+//  Created by Sau Chung Loh on 6/9/15.
+//  Copyright (c) 2015 Sau Chung Loh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
